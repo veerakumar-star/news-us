725dcb37f7027ad04cbfc648c6498062979f6a1d
veerakumar-star <veeravirat1881@gmail.com>
2026-04-21 11:00:56 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 import './App.css'
 import Navbar from './components/Navbar'
-import { Newsboard } from './components/Newsboard'
+import { Newsboard } from './components/NewsBoard'
 
 function App() {
   const [category, setCategory] = useState('general')
